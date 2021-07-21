8d8329b436ae7f81593103e01467c86bb04719d8
mamtawardhani <53176352+mamtawardhani@users.noreply.github.com>
2021-07-21 12:04:45 +0530

    Update Question.js

--- js/Question.js
+++ js/Question.js
@@ -5,6 +5,7 @@ class Question {
     this.input1 = createInput("Enter Your Name Here....");
     this.input2 = createInput("Enter Correct Option No..");
     this.button = createButton('Submit');
+    this.button2 = createButton('Reset');
     this.question = createElement('h3');
     this.option1 = createElement('h4');
     this.option2 = createElement('h4');
@@ -16,6 +17,7 @@ class Question {
     this.title.hide();
     this.input1.hide();
     this.button.hide();
+    //this.button2.hide();
     this.input2.hide();
   }
 
@@ -37,6 +39,7 @@ class Question {
     this.input1.position(150, 230);
     this.input2.position(350, 230);
     this.button.position(290, 300);
+    this.button2.position(360, 300);
 
     this.button.mousePressed(()=>{
       this.title.hide();
@@ -50,5 +53,12 @@ class Question {
       contestant.update();
       contestant.updateCount(contestantCount);
     });
+
+    this.button2.mousePressed(()=>{
+      contestant.updateCount(0)
+      quiz.update(0)
+      var ref = database.ref("contestants")
+      ref.remove()
+    })
   }
 }
